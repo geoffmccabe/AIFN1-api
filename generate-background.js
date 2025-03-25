c94de731c734b3894a23ac68428180836a8ea709
geoffmccabe <33639939+geoffmccabe@users.noreply.github.com>
2025-03-25 13:11:05 -0600

    Update generate-background.js

--- generate-background.js
+++ generate-background.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://geoffmccabe.github.io');
   res.header('Access-Control-Allow-Methods', 'GET, POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
